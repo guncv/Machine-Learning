5b322a950a3186f3bc810988be69f758be2911c7
Chanagun Viriyasathapornpong <chanagun.vir@gmail.com>
2024-01-19 16:43:37 +0700

    19/1/2024 04:43PM
    Machine Learning

--- ML1.py
+++ ML1.py
@@ -1,4 +1,6 @@
 from sklearn import datasets
+import matplotlib.pyplot as plt
+import pylab
 
 iris_dataset = datasets.load_iris()
 
@@ -8,3 +10,12 @@ print(iris_dataset['target'])
 print(iris_dataset['target_names'])
 print(iris_dataset['feature_names'])
 print(iris_dataset['data'][:10])
+
+digit_dataset = datasets.load_digits()
+
+print(digit_dataset.keys())
+print(digit_dataset['images'][:20])
+print(digit_dataset['target_names'])
+print(digit_dataset.images[0])
+pylab.imshow(digit_dataset.images[0],cmap=pylab.cm.gray_r)
+pylab.show()
